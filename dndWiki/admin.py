3b97c071a20539d7b75a56b01230bce3691d4c35
prox <prox@prox.com>
2021-06-29 23:20:17 -0400

    TRYING TO DEPLOY

---
+++ dndWiki/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+
+admin.site.register(Monster)
+admin.site.register(People)
+admin.site.register(Recap)
---
+++ dndWiki/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def peoplePage(request):
+    template = 'peoples.html'
+    context = {'peoples': People.objects.all(),
+               'person.name':People.name,
+               'person.race':People.race,
+               'person.description':People.description
+    }
+    return render(request,template,context)
+
+def recapPage(request):
+    template = 'recaps.html'
+    context = {'recaps': Recap.objects.all(),
+               'recap.date':Recap.date,
+               'recap.description':Recap.description
+    }
+    return render(request,template,context)
+
+def monsterPage(request):
+    template = 'monsters.html'
+    context = {'Monsters': Monster.objects.all(),'monster.name':Monster.name,'monster.type':Monster.type,'monster.description':Monster.description
+    }
+    return render(request,template,context)
